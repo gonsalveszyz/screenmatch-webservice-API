df3bb25e621679be21d362756b8f59e9864913a0
Arthur Gonçalves <oliveraarthurrtx@gmail.com>
2026-08-18 12:53:48 -0300

    implement movie search using OMDb API

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,13 +5,22 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+// URL API: "https://www.omdbapi.com/?i=tt3896198&apikey=930cbf45"
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = scanner.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?i=" + busca + "&apikey=930cbf45";
+
+        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=930cbf45"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
